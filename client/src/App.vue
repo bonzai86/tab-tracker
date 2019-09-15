179283f9fd097c6508fc3690e74f65fb102230c9
Nico Krueger <nico@itcare.local>
2019-09-15 19:33:34 +0200

    beautified with vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,18 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <PageHeader/>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import PageHeader from '@/components/Header.vue'
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    PageHeader
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Tab-Tracker</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
